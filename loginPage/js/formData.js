82ef13aa6fd4b53e1243b921cf114448e0c7d70f
Akshayakt <akshaya@qburst.com>
2015-12-15 16:18:29 +0530

    added new folder

---
+++ loginPage/js/formData.js
@@ -0,0 +1,44 @@
+function formData()
+{
+		var usrmail= document.getElementById("userEmail");  
+	    var paswd=document.getElementById("passWord");
+	    if(email_valid(usrmail))
+	    {
+	   		if (paswd_valid(paswd,7,20)) {
+	   		location.href = "hi.html?userEmail="+usrmail.value;
+			}
+		}
+		
+						 return false;
+}
+function email_valid(usrmail)  
+{  
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;  
+    if(usrmail.value.match(mailformat))  
+    {  
+        return true;  
+    }  
+    else  
+    {  
+        alert("You have to enter a valid email address!");  
+        usrmail.focus();  
+        return false;  
+    }  
+} 
+
+function paswd_valid(paswd,min,max)  
+{  
+    var passid_len = paswd.value.length;  
+    if (passid_len == 0 ||passid_len >= max || passid_len < min)  
+    {  
+    	alert("Password should not be empty / length be between "+min+" to "+max);  
+    	paswd.focus();  
+    	return false;  
+    }  
+    else{
+        alert("Access granted");
+        return true;  
+    }
+    
+}
+
